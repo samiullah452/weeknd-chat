6b824c32bc352f956d4a92ca5d10a4d0dff81085
Sami <m.sameeullahilyas@gmail.com>
2025-10-05 01:43:24 +0500

    Update

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const userService = require('../services/userService');
 const { MESSAGES } = require('../constants/messages');
 
 class AuthMiddleware {
-  static async authenticateSocket(socket, next) {
+  static authenticateSocket(socket, next) {
     try {
       const token = socket.handshake.auth?.token || socket.handshake.headers?.authorization?.replace('Bearer ', '');
       
@@ -11,25 +11,27 @@ class AuthMiddleware {
         return next(new Error(MESSAGES.ERROR.AUTH_TOKEN_REQUIRED));
       }
 
-      // const decoded = jwt.verify(token, process.env.JWT_SECRET, {
-      //   algorithms: ['HS512']
-      // });
+      jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        // if (err) {
+        //   return next(new Error(MESSAGES.ERROR.INVALID_TOKEN));
+        // }
 
-      try {
-        const decoded = {
-          id: 1622,
-          firstName: "kyle",
-          publicUser: 1,
-          hasAccess: true
+        try {
+          // decoded = {
+          //   id: 1622,
+          //   firstName: "kyle",
+          //   publicUser: 1,
+          //   hasAccess: true
+          // }
+          const userAccess = await userService.checkUserAccess(decoded);
+          
+          socket.userId = decoded.id;
+          socket.user = userAccess;
+          next();
+        } catch (userError) {
+          return next(new Error(userError.message));
         }
-        const userAccess = await userService.checkUserAccess(decoded);
-        
-        socket.userId = decoded.id;
-        socket.user = userAccess;
-        next();
-      } catch (userError) {
-        return next(new Error(userError.message));
-      }
+      });
     } catch (error) {
       next(new Error(MESSAGES.ERROR.AUTH_FAILED));
     }
